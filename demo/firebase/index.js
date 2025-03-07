d92b273b2913bb483ecbba05131cd27e5b1a6e7c
pacocho-programming <pacocho2525minion@gmail.com>
2025-03-07 18:07:35 +0900

    nothing

--- demo/firebase/index.js
+++ demo/firebase/index.js
@@ -1,7 +1,7 @@
 
   // Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app';
-import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.0.0/firebase-app.js';
+import { getAuth, onAuthStateChanged } from 'https://www.gstatic.com/firebasejs/9.0.0/firebase-auth.js';
 
 
 
