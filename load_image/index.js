ff85d2bc90804a0ceafa0581f0c6e670d80bd364
pacocho-programming <pacocho2525minion@gmail.com>
2025-03-12 13:38:06 +0900

    .

--- load_image/index.js
+++
@@ -1,4 +0,0 @@
-window.addEventListener('load', function() { // 画面の読み込みが完了したら
-	const element = document.querySelector('#loading'); // id="loading"に
-	element.classList.add('loadEnd'); // class="loadEnd"を付与する
-});
